78740a6a5508823354e38f88b1dd85788f0cde28
wajiha.rehman <wajiha.rehman@SD-001>
2022-10-13 10:53:24 +0500

    Initial Commit

---
+++ GettingStarted/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GettingStarted.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult StudentBio()
+        {
+            return View();
+        }
+    }
+}
---
+++ GettingStarted/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GettingStarted.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public string Code { get; set; }
+        public ICollection<StudentCourse> Enrollment { get; set; }
+
+    }
+}
